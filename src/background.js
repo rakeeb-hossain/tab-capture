48c9d8a0c933a8cc02165544d35f43d335e2e6d5
rakeeb123 <rakeeb.hossain1@gmail.com>
2019-11-15 18:18:44 -0500

    Saving and outputting URL's

--- src/background.js
+++ src/background.js
@@ -1,14 +1,28 @@
-// Called when the user clicks on the extension
+// Background Chrome API interactions
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.query({
-        currentWindow: true
-    }, function(tabs) {
-        chrome.tabs.query({
-            active:true,
-            currentWindow:true
-        }, function(curr) {
-            chrome.tabs.sendMessage(curr[0].id, {"message": "clicked_browser_action", "content": tabs});
-        });
-    });
-});
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.message === "open_tabs") {
+            console.log("opening...");
+        } else if (request.message === "get_tabs") {
+            chrome.tabs.query({
+                currentWindow: true
+            }, function(tabs) {
+                chrome.tabs.query({
+                    active:true,
+                    currentWindow:true
+                }, function(curr) {
+                    let tab_urls = [];
+                    for (var i = 0; i < tabs.length; i++) {
+                        tab_urls.push(tabs[i].url);
+                    }
+                    chrome.tabs.sendMessage(curr[0].id, {"message": "send_tabs", "content": tab_urls});
+                });
+            });
+        } else if (request.message === "load_tabs") {
+            chrome.storage.sync.get('tabs_1', function(tabs) {
+                console.log(tabs);
+            })
+        }
+    }
+)
--- src/content.js
+++ src/content.js
@@ -1,8 +1,31 @@
 // content.js
+
+// Save button event handler
+var saveButton = document.getElementById("saveButton");
+if (saveButton) {
+    saveButton.addEventListener("click", function(e) {
+        console.log("HELLO!");
+        chrome.runtime.sendMessage({"message": "get_tabs"});
+    });
+}
+
+// Load button event handler
+var loadButton = document.getElementById("loadButton");
+if (loadButton) {
+    loadButton.addEventListener("click", function(e) {
+        chrome.runtime.sendMessage({"message": "load_tabs"});
+    });
+}
+
+// Event listeners from background.js
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.message === "clicked_browser_action") {
             console.log(request.content);
+        } else if (request.message === "send_tabs") {
+            chrome.storage.sync.set({"tabs_1": request.content}, function() {
+                alert("Tabs saved!");
+            });
         }
     }
 )
